c294463a94575240f01510a9e7cd517348e8c5f6
tram ly <lyngoctramvns@gmail.com>
2023-11-21 23:20:57 +0700

    fetch apply to with particular value

--- fetchApplyTo.js
+++ fetchApplyTo.js
@@ -1,6 +1,8 @@
 import fetch from 'node-fetch';
 const domain = "test-tram-store-3.myshopify.com"
 const applyto = [0, 1, 2, 3, 4];
+const customerIds = [5906002247831,5962332340375];
+const customerTags = ["BSS_TAG","HAHAnowIgotyou"];
 
 const generalRule = {
     //import from a general file. File will be generated later
@@ -48,3 +50,39 @@ async function fetchApplyTo() {
     }
 }
 fetchApplyTo();
+
+async function fetchApplyToWithValue (){
+    const applyToRuleCustomer = {
+        data:{
+            ...generalRule,
+            name: "Testing Apply_To Field With Customer Id",
+            apply_to: 3,
+            customer_ids: customerIds
+        }
+    }
+
+    const applyToRuleTag = {
+        data: {
+            ...generalRule,
+            name: "Testing Apply_To Field With Customer Tag",
+            apply_to: 4,
+            customer_tags: customerTags
+        }
+    }
+
+    const responseCustomer = await fetch(`https://test-b2b-solution-api-21.test-bsscommerce.com/cp/rules/add?domain=${domain}`, {
+            method: 'put',
+            body: JSON.stringify(applyToRuleCustomer),
+            headers: { 'Content-Type': 'application/json' }
+    });
+    const responseApplyToCustomer = await responseCustomer.json();
+    const responseTag = await fetch(`https://test-b2b-solution-api-21.test-bsscommerce.com/cp/rules/add?domain=${domain}`, {
+            method: 'put',
+            body: JSON.stringify(applyToRuleTag),
+            headers: { 'Content-Type': 'application/json' }
+    });
+    const responseApplyToTag = await responseTag.json();
+    console.log(`Response to Apply To Fields Create Rule - With Customer Ids: ${JSON.stringify(responseApplyToCustomer)}`);
+    console.log('Response to Apply To Fields Create Rule - With Customer Tags: ' + JSON.stringify(responseApplyToTag));
+}
+fetchApplyToWithValue();
